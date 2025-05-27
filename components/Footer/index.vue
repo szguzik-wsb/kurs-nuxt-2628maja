dd5423f032c0f58a98a5914c15c13fef6917b717
Szymon Guzik <szymon.guzik@gdansk.merito.pl>
2025-05-27 07:02:40 +0200

    Pociecie szablonu

---
+++ components/Footer/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- Footer -->
+  <footer class="bg-green-600 text-white py-6 mt-12">
+    <div class="max-w-7xl mx-auto px-4 text-center">
+      <p>&copy; 2025 Green Allegro. Wszystkie prawa zastrzeżone.</p>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
